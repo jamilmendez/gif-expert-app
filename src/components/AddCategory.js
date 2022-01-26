6a1a26b6ca678b27734ac79135debe5af08c254e
Jamil Mendez <mdztnr99@gmail.com>
2022-01-26 16:51:19 -0500

    first commit gif-expert-app

---
+++ src/components/AddCategory.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+function AddCategory( {setCategories} ) {
+
+    const [inputValue, setInputValue] = useState('One Punch');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log('Submit hecho');
+        if (inputValue.trim().length > 2) {
+            setCategories(cats => [inputValue,...cats ]);
+            setInputValue('')
+        }
+    }
+
+    return (
+    <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            value={inputValue}
+            onChange={ handleInputChange }
+        />
+    </form>);
+}
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
+
+export default AddCategory;
